e1c636960e63aa02c62d1a3ee67a62e91286c5d7
PedroBarge <pedro525tavares@gmail.com>
2024-01-25 16:47:03 +0000

    done

--- mult.js
+++ mult.js
@@ -6,36 +6,39 @@ choose = [...choose];
 
 let calc = document.getElementById("calc");
 
-let clean=document.getElementById("clean");
+let clean = document.getElementById("clean");
 
 let result = document.getElementsByClassName("resultX0");
 let input = document.getElementById("input");
 let resultado = 0;
 let numOfDisplay;
 
-clean.addEventListener("click",()=>{
-    input.value="";
-})
+clean.addEventListener("click", () => {
+  input.value = "";
+  for (let i = 0; i < choose.length; i++) {
+    choose[i].innerText = "";
+  }
+  for (let i = 0; i < 11; i++) {
+    result[i].innerText = "";
+  }
+});
 
 option.forEach((num) => {
   num.addEventListener("click", () => {
-     numOfDisplay = input.value += num.innerText;
+    numOfDisplay = input.value += num.innerText;
   });
 });
 
-
-
-calc.addEventListener("click",()=>{
-    console.log(numOfDisplay);
-    for(let i=0;i<choose.length;i++){
-        choose[i].innerText= numOfDisplay;
-    }
-    for (let i = 0; i < 11; i++) {
-        resultado = numOfDisplay * i;
-        result[i].innerText = resultado;
-      }
+calc.addEventListener("click", () => {
+  console.log(numOfDisplay);
+  for (let i = 0; i < choose.length; i++) {
+    choose[i].innerText = numOfDisplay;
+  }
+  for (let i = 0; i < 11; i++) {
+    resultado = numOfDisplay * i;
+    result[i].innerText = resultado;
+  }
 });
-
 
 /*
 option.forEach((num) => {
